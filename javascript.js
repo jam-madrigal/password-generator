2f3ca11f19c45b604f6f02f13914316fad3dfde8
Alex Madrigal <jam.madrigal@gmail.com>
2020-06-24 14:32:10 -0700

    added several prompts/confirms

--- javascript.js
+++ javascript.js
@@ -1,16 +1,24 @@
-var criteria = {
+// All of the possible characters in a password
 
-    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    charCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    spec = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+var options = {
+
+    nums: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    char: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    charCaps: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    spec: ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"],
 }
 
 
 function pword() {
-    // Prompt the user to pick character count, do not allow < 8 or > 128
+    // Prompt the user to pick character count, do not allow < 8 or > 128, then ask about other criteria
+
     prompt("Enter password length from 8 to 128");
-        
+
+    confirm("Will the password contain special characters?");
+
+    confirm("Numbers?");
+
+    confirm("Capital letters?");
     
     
 
