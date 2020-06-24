662231ebe454f839fd06928bbff52234d3b91bfe
Alex Madrigal <jam.madrigal@gmail.com>
2020-06-24 15:12:20 -0700

    added some conditionals for the character type prompts and length prompts

--- javascript.js
+++ javascript.js
@@ -5,26 +5,33 @@ var options = {
     nums: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
     char: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
     charCaps: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-    spec: ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"],
+    spec: ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "`", "{", "|", "}", "~"],
 }
 
 
 function pword() {
     // Prompt the user to pick character count, do not allow < 8 or > 128, then ask about other criteria
 
-    prompt("Enter password length from 8 to 128");
+    length = prompt("Enter password length from 8 to 128");
 // display a random index the number of times of the input, only accepting 8-128
+    if (length < 8 || length > 128)
+    return alert("Password length must be between 8 and 128 characters")
 
-    confirm("Will the password contain special characters?");
+
+    special = confirm("Will the password contain special characters?");
 // if this is confirmed include random indexes from spec variable
 
-    confirm("Numbers?");
+    numbers = confirm("Numbers?");
 // if this is confirmed include random indexes from the nums variable
 
-    confirm("Capital letters?");
+    caps = confirm("Capital letters?");
     // if this is confirmed include random indexes from the charCaps variable
 
     // display a string pulling from the selected variable indexes, always including the char index
+
+    // Prompt the user to select at least one variable if none are selected
+    if ( special == false && numbers == false && caps == false)
+        return alert("Please choose at least one type of character to include.");
     
     
 
